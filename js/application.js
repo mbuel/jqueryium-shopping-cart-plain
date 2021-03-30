57ffea9cda417e1193923079b950aefc82f54818
mbuel <morris.buel@gmail.com>
2021-03-30 09:10:26 -0700

    initial layout completed

---
+++ js/application.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// template string 
+/*
+      <div class="row">
+        <div class="col-3">
+          <!-- Item name -->
+        </div>
+        <div class="col-3">
+          <!-- Unit Price -->
+        </div>
+        <div class="col-3">
+            <!-- Quanity -->
+        </div>
+        <div class="col-3">
+          <!-- item remove -->
+          <div class="row">
+            <div class="total col-6">
+              $ _______
+            </div>
+            <div class="col-6">
+              <div class="btn-remove btn-danger text-center rounded-pill">remove</div>
+            </div>
+          </div>
+        </div>
+      </div>
+*/
+
