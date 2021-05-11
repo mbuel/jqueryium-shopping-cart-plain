379510928add3c86101da269c3d23aa3df666e55
mbuel <morris.buel@gmail.com>
2021-05-10 23:50:41 -0700

    changed framework to bulma, removed JQUERY - need to finish listeners

--- js/application.js
+++ js/application.js
@@ -1,48 +1,110 @@
 'use strict';
 
+let totalQty = undefined;
+let totalCost = undefined;
+let remove = undefined;
+let add = undefined;
+let price = undefined;
+let qty = undefined;
+
+
+// Currency Formatter
+// https://flaviocopes.com/how-to-format-number-as-currency-javascript/
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
+const updateListeners = () => {
+  remove = Array.from(document.querySelectorAll('.grocery-item .remove'));
+
+  remove.forEach(el => {
+    el.addEventListener('click', function (el) {
+      const parentEl = findParentBySelector(el.target, '.columns.grocery-item');
+
+      console.log(parentEl);
+      parentEl.parentNode.removeChild(parentEl);
+      updateTotalCosts();
+      updateListeners();
+
+    });
+  });
+
+
+  price = Array.from(document.querySelectorAll('.cost input'));
+  price.forEach(el => {
+    el.addEventListener('input', function(e) {
+      updateTotalCosts();
+      console.log(e);
+    });
+  });
+  qty = Array.from(document.querySelectorAll('.qty input'));
+  
+  add = document.querySelector('.add').addEventListener('click', addItemToList);
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  totalQty = document.querySelector('#total-items');
+  totalCost = document.querySelector('#total-cost');
+
+  updateTotalCosts();
+  updateListeners();
+
+
+  const el = document.createElement('div');
+document.querySelector('body').appendChild(el)
+
+  var timeout;
+  var updateList = function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+        updateTotalCosts();
+    }, 250);
+  }
+
+
+});
+
 // template string 
 var templateString = `<!-- Template Row to add per item -->
-      <div class="row grocery-item text-center">
-        <div class="item-name col-3">
+      <div class="columns grocery-item is-centered">
+        <div class="item-name column is-one-quarter">
           <!-- item name -->
           %ITEM%
         </div>
-        <div class="cost col-3">
+        <div class="cost column is-one-quarter">
           <!-- item price -->
           <input type="number" value="%PRICE%" />
         </div>
-        <div class="qty col-3">
+        <div class="qty column is-one-quarter">
           <!-- item quantity -->
           <input type="number" value="%QTY%" />
         </div>
-        <div class="col-3">
+        <div class="column is-one-quarter">
           <!-- item remove -->
-          <div class="summary row">
-            <div class="total col-6 text-success">
+          <div class="summary columns">
+            <div class="total column is-one-half is-success">
               %TOTAL%
             </div>
-            <div class="col-6">
-              <div class="btn remove btn-danger text-center rounded-pill"><i class="fa fa-remove"></i></div>
+            <div class="column is-one-half">
+              <div class="button remove is-danger is-centered is-rounded"><i class="fa fa-remove"></i></div>0
             </div>
           </div>
         </div>
       </div>`;
 
-// Currency Formatter
-// https://flaviocopes.com/how-to-format-number-as-currency-javascript/
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  })
+
   
 
 var updateRowTotalCost = function(row) {
-    var itemQty = parseFloat($(row).find('.qty input').val());
-    var itemCost = parseFloat($(row).find('.cost input').val());
+    console.log(row);
 
-    var rowCostTotal = itemQty * itemCost;
-    $(row).find('.total').text(formatter.format(rowCostTotal));
+    const itemCost = row.querySelector('.cost input').value;
+    const itemQty = row.querySelector('.qty input').value;
+
+    const rowCostTotal = itemQty * itemCost;
     
     return {
         rowCostTotal: rowCostTotal,
@@ -53,20 +115,27 @@ var updateRowTotalCost = function(row) {
 var updateTotalCosts = function() {
     var totalCost = 0;
     var totalItems = 0;
-    $('.list > .grocery-item').each(function(i, ele) {
-        var {rowCostTotal, itemQty} = updateRowTotalCost(ele);
+    const items = Array.from(document.querySelectorAll('.grocery-item'));
+      items.forEach(ele => {
+        const {rowCostTotal, itemQty} = updateRowTotalCost(ele);
+        ele.querySelector('.total').textContent = formatter.format(rowCostTotal);
         console.log(rowCostTotal, itemQty);
-        totalItems += itemQty;
-        totalCost += rowCostTotal;
+        totalItems += Number(itemQty);
+        totalCost += Number(rowCostTotal);
     });
-    $('#total-items').text(Math.floor(totalItems));
-    $('#total-cost').text(formatter.format(totalCost));
+
+    document.querySelector('#total-items').textContent = totalItems;
+    document.querySelector('#total-cost').textContent = formatter.format(Number(totalCost));
+
 }
 
 var addItemToList = function() {
-    var item = $('#new-item').val();
-    var qty = $('#new-qty').val();
-    var cost = $('#new-cost').val();
+  const newItem = document.querySelector('#new-item');
+  const newQty = document.querySelector('#new-qty');
+  const newCost = document.querySelector('#new-cost');
+    var item = newItem.value;
+    var qty = newQty.value;
+    var cost = newCost.value;
     
     console.log(item, qty, cost);
 
@@ -78,35 +147,46 @@ var addItemToList = function() {
     }
 
     var item = templateString.replace(/%ITEM%/, item).replace(/%QTY%/, qty).replace(/%PRICE%/, cost);
-
-    $('.list').append(item);
+    document.querySelector('.list').append(item);
+    // $('.list').append(item);
 
     updateTotalCosts();
 }
 
 
-var timeout;
-var updateList = function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-        updateTotalCosts();
-    }, 250);
+
+
+// $(document).ready(function () {
+//     updateTotalCosts();
+    
+//     $('.list').parent().on('click', '.remove', function (event) {
+
+//         $(this).parent().parent().parent().parent().remove();
+//         updateTotalCosts();
+
+//     });
+    
+//     $('.list').on('input', updateList);
+
+//     $('.add').on('click', addItemToList); 
+
+
+
+// });
+
+function collectionHas(a, b) { //helper function (see below)
+  for(var i = 0, len = a.length; i < len; i ++) {
+      if(a[i] == b) return true;
+  }
+  return false;
 }
 
-$(document).ready(function () {
-    updateTotalCosts();
-    
-    $('.list').parent().on('click', '.remove', function (event) {
-
-        $(this).parent().parent().parent().parent().remove();
-        updateTotalCosts();
-
-    });
-    
-    $('.list').on('input', updateList);
-
-    $('.add').on('click', addItemToList); 
-
-
-
-});
+function findParentBySelector(dom, selector) {
+  var all = document.querySelectorAll(selector);
+  var cur = dom.parentNode;
+  console.log(cur, dom, selector, all);
+  while(cur && !collectionHas(all, cur)) { //keep going up until you find a match
+      cur = cur.parentNode; //go up
+  }
+  return cur; //will return null if not found
+}
